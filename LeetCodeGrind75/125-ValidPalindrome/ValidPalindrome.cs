8a328e5fd9c92958e97c88962464bc5d33b36afd
Emre GÜZEL <emrreguzel@gmail.com>
2024-03-02 12:52:26 +0300

    LeetCode 125 is solved.

---
+++ LeetCodeGrind75/125-ValidPalindrome/ValidPalindrome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeGrind75._125_ValidPalindrome
+{
+    public class Solution125
+    {
+        public bool IsPalindrome(string s)
+        {
+            var filtered = "";
+            foreach (var c in s)
+            {
+                if (Char.IsLetterOrDigit(c))
+                {
+                    filtered += Char.ToLower(c);
+                }
+            }
+            char[] charArray = filtered.ToCharArray();
+            Array.Reverse(charArray);
+            var reversedString = new string(charArray);
+            return (filtered.Equals(reversedString)) ? true : false;
+        }
+    }
+}
