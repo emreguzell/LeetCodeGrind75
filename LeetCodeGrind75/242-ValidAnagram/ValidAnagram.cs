3ebeed6a25481221f30aa1d7d8405a9a97a7ae44
Emre GÜZEL <emrreguzel@gmail.com>
2024-03-13 00:21:43 +0300

    LeetCode 242 has been solved.

---
+++ LeetCodeGrind75/242-ValidAnagram/ValidAnagram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeGrind75._242_ValidAnagram
+{
+    public class Solution242
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length < 1 || t.Length < 1 || s.Length != t.Length || s == null || t == null)
+            {
+                return false;
+            }
+            char[] s1 = s.ToCharArray();
+            char[] t1 = t.ToCharArray();
+            Array.Sort(s1);
+            Array.Sort(t1);
+            string s2 = new string(s1);
+            string t2 = new string(t1);
+            return s2 == t2;
+        }
+    }
+}
