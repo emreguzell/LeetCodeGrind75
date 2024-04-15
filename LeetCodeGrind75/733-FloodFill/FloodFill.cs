5a39cc3b12f843c8a499d916f63a256abf6a5b92
Emre GÜZEL <emrreguzel@gmail.com>
2024-04-15 09:46:45 +0300

    LeetCode733 has been solved.

---
+++ LeetCodeGrind75/733-FloodFill/FloodFill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeGrind75._733_FloodFill
+{
+    public class Solution733
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            // Check if the new color is the original color
+            if (image[sr][sc] != newColor)
+            {
+                DepthFirstSearch(image, sr, sc, image[sr][sc], newColor);
+            }
+            return image;
+        }
+        // Depth First Search Algorithm
+        private void DepthFirstSearch(int[][] image, int sr, int sc, int originalColor, int newColor)
+        {
+            if (sr < 0 || sr >= image.Length || sc < 0 || sc >= image[0].Length || image[sr][sc] != originalColor)
+            {
+                return;
+            }
+            image[sr][sc] = newColor;
+            DepthFirstSearch(image, sr + 1, sc, originalColor, newColor);
+            DepthFirstSearch(image, sr - 1, sc, originalColor, newColor);
+            DepthFirstSearch(image, sr, sc + 1, originalColor, newColor);
+            DepthFirstSearch(image, sr, sc - 1, originalColor, newColor);
+        }
+    }
+}
