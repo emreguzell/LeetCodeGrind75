4887c9afe2825e773b30ebf64abbd666c28b9220
Emre GÜZEL <emrreguzel@gmail.com>
2024-04-16 10:51:02 +0300

    LeetCode 235 has been solved.

---
+++ LeetCodeGrind75/235-LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeGrind75._226_InvertBinaryTree.Solution226;
+
+namespace LeetCodeGrind75._235_LowestCommonAncestorOfABinarySearchTree
+{
+    public class Solution235
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (p.val > root.val && q.val > root.val)
+            {
+                return LowestCommonAncestor(root.right, p, q);
+            }
+            else if (p.val < root.val && q.val < root.val)
+            {
+                return LowestCommonAncestor(root.left, p, q);
+            }
+            else
+            {
+                return root;
+            }
+        }
+        public class TreeNode 
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
