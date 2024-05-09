c7bd3528e76be81d2a7424e27ca9606d77d6fff6
Emre GÜZEL <emrreguzel@gmail.com>
2024-05-09 19:38:21 +0300

    LeetCode 141 has been solved.

---
+++ LeetCodeGrind75/141-LinkedListCycle/LinkedListCycle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeGrind75._021_MergeTwoSortedLists.Solution021;
+
+namespace LeetCodeGrind75._141_LinkedListCycle
+{
+	public class Solution141
+	{
+
+		public bool HasCycle(ListNode head)
+		{
+			HashSet<ListNode> seenNodes = new HashSet<ListNode>();
+			while (head != null)
+			{
+				if (seenNodes.Contains(head))
+				{
+					return true;
+				}
+				seenNodes.Add(head);
+				head = head.next;
+			}
+			return false;
+		}
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x)
+			{
+				val = x;
+				next = null;
+			}
+		}
+	}
+}
