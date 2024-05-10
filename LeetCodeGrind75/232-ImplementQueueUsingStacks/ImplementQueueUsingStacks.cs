0b9377c15e68644955ae9f6aee4bc2ca0b927325
Emre GÜZEL <emrreguzel@gmail.com>
2024-05-10 09:41:50 +0300

    LeetCode 232 has been solved.

---
+++ LeetCodeGrind75/232-ImplementQueueUsingStacks/ImplementQueueUsingStacks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeGrind75._232_ImplementQueueUsingStacks
+{
+	public class Solution232
+	{
+		public class MyQueue
+		{
+			private Stack<int> stackIn;
+			private Stack<int> stackOut;
+
+			public MyQueue()
+			{
+				stackIn = new Stack<int>();
+				stackOut = new Stack<int>();
+			}
+
+			public void Push(int x)
+			{
+				stackIn.Push(x);
+			}
+
+			public int Pop()
+			{
+				if (stackOut.Count == 0)
+				{
+					while (stackIn.Count > 0)
+					{
+						stackOut.Push(stackIn.Pop());
+					}
+				}
+				return stackOut.Pop();
+			}
+
+			public int Peek()
+			{
+				if (stackOut.Count == 0)
+				{
+					while (stackIn.Count > 0)
+					{
+						stackOut.Push(stackIn.Pop());
+					}
+				}
+				return stackOut.Peek();
+			}
+
+			public bool Empty()
+			{
+				return (stackIn.Count == 0 && stackOut.Count == 0);
+			}
+		}
+
+	}
+}
+
+//Your MyQueue object will be instantiated and called as such:
+// MyQueue obj = new MyQueue();
+//obj.Push(x);
+//int param_2 = obj.Pop();
+//int param_3 = obj.Peek();
+//bool param_4 = obj.Empty();
